a4be8a0debb433e836cffc25d67a3b274a99d224
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-11 00:09:48 +0100

    updated

--- has12.js
+++ has12.js
@@ -10,8 +10,12 @@
 
 function has12(nums){
     for(let i = 0; i < nums.length;i++){
-        if(nums[i] === 1 || nums[i]=== 2){
-            return 'true'
+        if(nums[i] === 1){
+           for(let j = i + 1; j < nums.length; j++){
+            if(nums[j] === 2){
+                return true
+            }
+           }
         }
-    }return 'false'
+    }return false
   }console.log(has12([3,1,2]));
